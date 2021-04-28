f879ff61d74234af7215c7f5c9650f2e2952205b
Piotr Zawadzki <piotr.98zawadzki@gmail.com>
2021-04-28 11:10:49 +0200

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/pl/nauka/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/nauka/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pl.nauka.sfgpetclinic.model;
 
-public class Person {
+public class Person  extends  BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/pl/nauka/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/nauka/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pl.nauka.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
