61d4b26a204685a48e84f55f34a20634e5463a43
Piotr Zawadzki <piotr.98zawadzki@gmail.com>
2021-05-09 14:04:01 +0200

    List all Owners and Vets on Owner index page and Vet index page. Closes #14

--- pet-clinic-web/src/main/java/pl/nauka/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/nauka/sfgpetclinic/controllers/OwnerController.java
@@ -1,11 +1,24 @@
 package pl.nauka.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.nauka.sfgpetclinic.services.OwnerService;
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    @RequestMapping({"/owners","owners/index.html","owners/index"})
-    public String listOwners(){
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+
+    @RequestMapping({"index.html","/index"})
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index.html";
     }
 }
--- pet-clinic-web/src/main/java/pl/nauka/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/nauka/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,24 @@
 package pl.nauka.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.nauka.sfgpetclinic.services.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets","vets/index.html","vets/index"})
-    public String listVets(){
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    private  final VetService vetService;
+
+
+    @RequestMapping({"index.html","/index"})
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index.html";
     }
 }
